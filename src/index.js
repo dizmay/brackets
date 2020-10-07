70d2c67024b2e45176b02467d5ed33e84db6aa0b
dizmay <hauntfire1@gmail.com>
2020-10-07 22:56:53 +0300

    refactor: change equals

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
   let count = 1;
-  while(count == 1) {
+  while(count === 1) {
       let newStr = str;
       bracketsConfig.map(e => str = str.replace(e[0] + e[1], ''));
       if (newStr === str) count = 0;
   }
-  return str.length == 0
+  return str.length === 0
 }
